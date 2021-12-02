36f86e1aad16953bb45822c89deb7835978637b5
Martijn Michel <info@tocadovision.nl>
2021-12-02 12:44:06 +0100

    df

--- src/routes/chord.js
+++ src/routes/chord.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 import { Chord } from '@tonaljs/modules';
 import { generateGuitarChord } from '../scripts/generateChord';
 import { ChordType } from '@tonaljs/tonal';
+import { getVoicingsFromChord } from '@martijnmichel/chordshape';
 
 const router = Router();
 
@@ -14,6 +15,15 @@ router.get('/:root/:chordAlias', (req, res) => {
   return res.send(
     Chord.chord(`${req.params.root}${req.params.chordAlias}`),
   );
+});
+
+router.get('/:root/:chroma/variants', async (req, res) => {
+  const chord = ChordType.all().find(
+    (c) => c.chroma === req.params.chroma,
+  );
+
+  const data = getVoicingsFromChord(chord.aliases[0]);
+  return res.send(data);
 });
 
 router.get('/:root/:chroma/png', async (req, res) => {
